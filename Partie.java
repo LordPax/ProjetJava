96531c5d8b7b83e8066c0cf3ffb1eff18361c1f8
LordPompax <capitaine_ted@hotmail.fr>
2019-06-06 22:20:54 +0200

    vérification de la validité des déplacement de toute les pieces sauf le cavalier

--- Partie.java
+++ Partie.java
@@ -9,13 +9,11 @@ public class Partie{
 		this.echequier = new Echequier();
 	}
 	public void lancerPartie(){
-		this.echequier.setEchec(3, 1, 1, 0);
-		this.echequier.setEchec(3, 6, 1, 1);
+		this.echequier.setEchec(3, 1, 4, 0);
+		this.echequier.setEchec(3, 6, 4, 1);
 		this.afficher();
-		System.out.println(this.deplacerPiece(3, 1, 3, 3, 0));
-		//System.out.println(this.deplacerPiece(3, 6, 3, 4, 1));
+		System.out.println(this.deplacerPiece(3, 6, 3, 6, 1));
 		this.afficher();
-
 
 		// System.out.println(this.deplacerPiece(1, 1, 1, 3, 0)); // déplacement d'une piece
 		// this.afficher();
@@ -41,12 +39,45 @@ public class Partie{
 		int num = this.echequier.getEchec(x, y).getNumPiece(); // "copie" la piece au coord initial
 		int couleur = this.echequier.getEchec(x, y).getCouleur(); // copie la couleur
 		int dep = 0, deb0 = 0, deb1 = 0;
+		int rx = 0, ry = 0;
 
 		if (x >= 0 && x < 8 && y >= 0 && y < 8 && dx >= 0 && dx < 8 && dy >= 0 && dy < 8 && (joueur == 0 || joueur == 1)) {
 			if (num != 0){
 				if (couleur == joueur) {
 					switch (num) {
-						case 1 :
+						case 1 : // Roi
+							rx = dx - x; // coord relatif x
+							ry = dy - y; // coord relatif y
+							if (Math.abs(rx) <= 1 && Math.abs(ry) <= 1){
+								this.echequier.setEchec(x, y, 0, couleur); // remplace la piece au coord initial par une piece null
+								this.echequier.setEchec(dx, dy, num, couleur); // remplace la piece null au coord d'arriver par la piece copier au par avant
+							}
+							else{
+								return 4; // retourne 4 si le déplacement est invalide pour cette piece
+							}
+							break;
+
+						case 2 : // Dame
+							if (x == dx || y == dy || Math.abs(dx - x) == Math.abs(dy - y)){
+								this.echequier.setEchec(x, y, 0, couleur); // remplace la piece au coord initial par une piece null
+								this.echequier.setEchec(dx, dy, num, couleur); // remplace la piece null au coord d'arriver par la piece copier au par avant
+							}
+							else{
+								return 4;
+							}
+							break;
+
+						case 3 : // Tour
+							if (x == dx || y == dy){
+								this.echequier.setEchec(x, y, 0, couleur); // remplace la piece au coord initial par une piece null
+								this.echequier.setEchec(dx, dy, num, couleur); // remplace la piece null au coord d'arriver par la piece copier au par avant
+							}
+							else{
+								return 4;
+							}
+							break;
+
+						case 4 : // Cavalier
 							if (true){
 								this.echequier.setEchec(x, y, 0, couleur); // remplace la piece au coord initial par une piece null
 								this.echequier.setEchec(dx, dy, num, couleur); // remplace la piece null au coord d'arriver par la piece copier au par avant
@@ -56,10 +87,20 @@ public class Partie{
 							}
 							break;
 
-						case 6 :
-							dep = (couleur == 0) ? 1 : -1;
-							deb0 = (y == 1 && couleur == 0) ? 2 : 1;
-							deb1 = (y == 6 && couleur == 1) ? 2 : 1;
+						case 5 : // Fou
+							if (Math.abs(dx - x) == Math.abs(dy - y)){
+								this.echequier.setEchec(x, y, 0, couleur); // remplace la piece au coord initial par une piece null
+								this.echequier.setEchec(dx, dy, num, couleur); // remplace la piece null au coord d'arriver par la piece copier au par avant
+							}
+							else{
+								return 4;
+							}
+							break;
+
+						case 6 : // Pion
+							dep = (couleur == 0) ? 1 : -1; // permet de ce déplacer dans un sens ou dans l'autre en fonction de la valeur de la couleur du pion
+							deb0 = (y == 1 && couleur == 0) ? 2 : 1; // permet à un pion blanc de pouvoir avancer de 2 case si le pion est a son point départ
+							deb1 = (y == 6 && couleur == 1) ? 2 : 1; // idem mais pour les pion noir
 
 							if (x == dx && ((dy - y) == deb0 * dep || (dy - y) == deb1 * dep)){
 								this.echequier.setEchec(x, y, 0, couleur); // remplace la piece au coord initial par une piece null
@@ -71,7 +112,6 @@ public class Partie{
 							break;
 					}
 					return 0; // retourne 0 si tout ce passe bien
-					
 				}
 				else{
 					return 1; // retourne 1 si la couleur du joueur est différente de la couleur de la piece choisi
