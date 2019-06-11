2cdcd424a729b0b5b97664f1d7396fc52bea1634
LordPompax <capitaine_ted@hotmail.fr>
2019-06-12 01:19:04 +0200

    maj pas fini

--- Joueur.java
+++ Joueur.java
@@ -4,7 +4,22 @@ public class Joueur{
 	private int couleur;
 	private int pts;
 
-	public Joueur(){
-		
+	public Joueur(int c){
+		this.couleur = c;
+		this.pts = 0;
+	}
+
+	public int getPts(){
+		return this.pts;
+	}
+	public int getCouleur(){
+		return this.couleur;
+	}
+
+	public void setPts(int pts){
+		this.pts = pts;
+	}
+	public void setCouleur(int c){
+		this.couleur = c;
 	}
 }
--- Partie.java
+++ Partie.java
@@ -2,19 +2,26 @@ import java.util.ArrayList;
 
 public class Partie{
 	private int tour; // nombre de tour éffectuer par les joueurs
-	private Joueur[] joueur; // joueurs au nombre de 2
+	private ArrayList<Joueur> joueur; // joueurs au nombre de 2
 	private Echequier echequier; // //échéquier ou ce trouve les piceces 
 
 	public Partie(){
+		// this.joueur.add(new Joueur(0));
+		// this.joueur.add(new Joueur(1));
 		this.echequier = new Echequier();
 	}
+
+	public Joueur getJoueur(int c){
+		return this.joueur.get(c);
+	}
+
 	public void lancerPartie(){
 		//this.echequier.setEchec(3, 1, 2, 0);
-		this.echequier.setEchec(3, 4, 4, 1);
-		this.echequier.setEchec(2, 2, 6, 1);
+		this.echequier.setEchec(3, 6, 2, 1);
+		this.echequier.setEchec(3, 1, 6, 1);
 		
 		this.afficher();
-		System.out.println(this.deplacerPiece(3, 4, 4, 2, 1));
+		System.out.println(this.deplacerPiece(3, 6, 3, 1, 1));
 		this.afficher();
 
 		// System.out.println(this.deplacerPiece(1, 1, 1, 3, 0)); // déplacement d'une piece
@@ -45,23 +52,19 @@ public class Partie{
 			if (num != 0){
 				if (couleur == joueur) {
 					if (this.trajOk(x, y, dx, dy) == 0) {	
-						return deplacementValide(x, y, dx, dy, joueur, num, couleur);
+						return this.deplacementValide(x, y, dx, dy, joueur, num, couleur);
 					}
-					else{
+					else
 						return 5; // retourne 5 si il y un obstacle sur le trajet
-					}
 				}
-				else{
+				else
 					return 1; // retourne 1 si la couleur du joueur est différente de la couleur de la piece choisi
-				}
 			}
-			else{
+			else
 				return 2; // retourne 2 si il n'y a aucune piece su endroit choisi
-			}
 		}
-		else{
+		else
 			return 3; // retourne 3 si les valeur d'entrer son invalide
-		}
 	}
 	public int deplacementValide(int x, int y, int dx, int dy, int joueur, int num, int couleur){
 		int dep = 0, deb0 = 0, deb1 = 0;
@@ -70,33 +73,28 @@ public class Partie{
 
 		switch (num) {
 			case 1 : // Roi
-				if (!(Math.abs(rx) <= 1 && Math.abs(ry) <= 1)){
+				if (!(Math.abs(rx) <= 1 && Math.abs(ry) <= 1))
 					return 4; // retourne 4 si le déplacement est invalide pour cette piece
-				}
 				break;
 
 			case 2 : // Dame
-				if (!(x == dx || y == dy || Math.abs(rx) == Math.abs(ry))){
+				if (!(x == dx || y == dy || Math.abs(rx) == Math.abs(ry)))
 					return 4; // retourne 4 si le déplacement est invalide pour cette piece
-				}
 				break;
 
 			case 3 : // Tour
-				if (!(x == dx || y == dy)){
+				if (!(x == dx || y == dy))
 					return 4; // retourne 4 si le déplacement est invalide pour cette piece
-				}
 				break;
 
 			case 4 : // Cavalier
-				if (!(Math.abs(rx) == 1 && Math.abs(ry) == 2 || Math.abs(rx) == 2 && Math.abs(ry) == 1)){
+				if (!(Math.abs(rx) == 1 && Math.abs(ry) == 2 || Math.abs(rx) == 2 && Math.abs(ry) == 1))
 					return 4; // retourne 4 si le déplacement est invalide pour cette piece
-				}
 				break;
 
 			case 5 : // Fou
-				if (!(Math.abs(rx) == Math.abs(ry))){
+				if (!(Math.abs(rx) == Math.abs(ry)))
 					return 4; // retourne 4 si le déplacement est invalide pour cette piece
-				}
 				break;
 
 			case 6 : // Pion
@@ -104,9 +102,8 @@ public class Partie{
 				deb0 = (y == 1 && couleur == 0) ? 2 : 1; // permet à un pion blanc de pouvoir avancer de 2 case si le pion est a son point départ
 				deb1 = (y == 6 && couleur == 1) ? 2 : 1; // idem mais pour les pion noir
 
-				if (!(x == dx && (rx == deb0 * dep || ry == deb1 * dep))){
+				if (!(x == dx && (rx == deb0 * dep || ry == deb1 * dep)))
 					return 4; // retourne 4 si le déplacement est invalide pour cette piece
-				}
 				break;
 		}
 
@@ -117,35 +114,46 @@ public class Partie{
 	public int trajOk(int x, int y, int dx, int dy){
 		int rx = dx - x; // coord relatif x
 		int ry = dy - y; // coord relatif y
-		int num = this.echequier.getEchec(x, y).getNumPiece();;
+		int num = this.echequier.getEchec(x, y).getNumPiece();
+		int couleurInit = this.echequier.getEchec(x, y).getNumPiece();
 		int dirx = 0, diry = 0;
 		int coordx = 0, coordy = 0;
 
-		if (rx < 0) {dirx = -1;}
-		else if (rx > 0) {dirx = 1;}
-		else {dirx = 0;}
+		if (rx < 0) dirx = -1;
+		else if (rx > 0) dirx = 1;
+		else dirx = 0;
 
-		if (ry < 0) {diry = -1;}
-		else if (ry > 0) {diry = 1;}
-		else {diry = 0;}
+		if (ry < 0) diry = -1;
+		else if (ry > 0) diry = 1;
+		else diry = 0;
 
 		if (num != 4){
-			for (int i = 1; i <= Math.abs(rx) || i <= Math.abs(ry); i++) {
+			for (int i = 1; i < Math.abs(rx) || i < Math.abs(ry); i++) {
 				coordx = i * dirx;
 				coordy = i * diry;
 				num = this.echequier.getEchec(x + coordx, y + coordy).getNumPiece();
-				if (num != 0) {
+				if (num != 0) 
 					return 1;
-				}
+					// if (x + coordx == dx && y + coordy == dy) {
+					// 	couleurAutrePiece = this.echequier.getEchec(x + coordx, y + coordy).getNumPiece();
+					// 	return this.prendrePiece(dx, dy, couleurInit, couleurAutrePiece);
+					// }
+					// else
 			}
 		}
 		else{
 			num = this.echequier.getEchec(dx, dy).getNumPiece();
-			if (num != 0) {
+			if (num != 0)
 				return 1;
-			}
 		}
 
 		return 0;
+	}
+	public int prendrePiece(int x, int y, int joueur, int couleurAutrePiece){
+		if (joueur == couleurAutrePiece) {
+			return 0;
+		}
+		else
+			return 1;
 	}
 }
